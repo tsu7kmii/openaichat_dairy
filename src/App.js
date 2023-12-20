7edea470e65cb8a895966902dad5f14e43185f9a
tsu7 <kmiibfw@gmail.com>
2023-12-20 12:31:53 +0900

    flaskのバックエンドまで仮完了

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
+  const sendMessage = async () => {
+    if (input === '') {
+      return;
+    }
+    const newMessage = {
+      role: 'user',
+      content: input
+    };
+    setMessages([...messages, newMessage]);
+    setInput('');
+    // ここでメッセージを送信し、応答を処理します
+    // この部分は後で実装します
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chat with AI</h1>
       </header>
+      <div className="chat-window">
+        {messages.map((msg, index) => (
+          <p key={index}>{msg.content}</p>
+        ))}
+      </div>
+      <div className="input-area">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
