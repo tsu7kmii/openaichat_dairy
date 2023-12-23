23ddb1833d4071f0b010762df9c9cdff09166c88
tsu7 <kmiibfw@gmail.com>
2023-12-23 13:47:52 +0900

    perfect

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import './InputArea.css'; // CSSファイルのインポート
 
 const InputArea = ({ input, setInput, sendMessage }) => {
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault(); // デフォルトのEnter挙動（改行）を防止
+      sendMessage();
+    }
+  };
+
   return (
-    <div className="input-area">
-      <input
-        type="text"
+    <div className="input-area-container">
+      <textarea
+        className="input-area"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyPress={handleKeyPress}
+        rows="1"
+        placeholder="Type your message here..."
       />
-      <button onClick={sendMessage}>Send</button>
+      <button className="send-button" onClick={sendMessage}>Send</button>
     </div>
   );
 };
